1594dadffdb059f42e2a055cb29873af8b9115bc
davidsolorzano1 <davidbs1201@hotmail.com>
2019-01-05 13:39:58 -0700

    lo del gallo y alexis alv

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var taskRouter = require('./routes/task');
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tasks', taskRouter);
 
 module.exports = app;
